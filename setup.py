0ee226c77330bc1473f7bd4cf33a3c7a48484544
davidgang <dgang@freightos.com>
2018-03-19 20:38:06 +0200

    initial commit and tests

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='template-file-parser',
+      version='0.1',
+      description='Tiny wrapper over string templates which works with files',
+      url='https://github.com/david-gang/template-file-parser',
+      author='David Gang',
+      author_email='michaelgang@gmail.com',
+      license='MIT',
+      packages=['template-file-parser'],
+      python_requires=">=3.4",
+      zip_safe=False)
