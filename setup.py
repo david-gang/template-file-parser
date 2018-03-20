3cf2b935205dd91b209e97f6f7e5f92e83a24271
davidgang <dgang@freightos.com>
2018-03-20 12:51:11 +0200

    fix version

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
-setup(name='template-file-parser',
-      version='0.1',
-      description='Tiny wrapper over string templates which works with files',
-      url='https://github.com/david-gang/template-file-parser',
-      author='David Gang',
-      author_email='michaelgang@gmail.com',
-      license='Apache 2.0',
-      packages=['template_file_parser'],
-      python_requires=">=3.4",
-      zip_safe=False)
+setup(
+    name='template-file-parser',
+    version='0.11',
+    description='Tiny wrapper over string templates which works with files',
+    url='https://github.com/david-gang/template-file-parser',
+    author='David Gang',
+    author_email='michaelgang@gmail.com',
+    license='Apache 2.0',
+    packages=['template_file_parser'],
+    python_requires=">=3.4",
+    zip_safe=False)
