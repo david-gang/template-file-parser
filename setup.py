3804c7cacd467fa3eddf32046689e4916638158b
davidgang <dgang@freightos.com>
2018-03-20 13:02:56 +0200

    include rst in pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+import os
 from setuptools import setup
+
+
+def readme():
+    with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+        return f.read()
+
 
 setup(
     name='template-file-parser',
-    version='0.13',
+    version='0.14',
     description='Tiny wrapper over string templates which works with files',
+    long_description=readme(),
     url='https://github.com/david-gang/template-file-parser',
     author='David Gang',
     author_email='michaelgang@gmail.com',
